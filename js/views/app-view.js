71d91fbdfca1e4a9c7dbf3dc8d9ecbf69eb8c74b
Kent Bute <kent.bute@icrossing.com>
2016-08-11 21:36:51 -0700

    make draggable

--- js/views/app-view.js
+++ js/views/app-view.js
@@ -6,7 +6,10 @@ var app = app || {};
 	app.AppView = Backbone.View.extend({
 		el: '.main-app',
 		events: {
-			'click button.create-note': 'newNote'
+			'click button.create-note': 'newNote',
+			'dragstart .content': 'dragStart',
+			'dragover .content': 'dragOver',
+			'drop .content': 'dragDrop'
 		},
 		initialize: function () {
 			this.$content = this.$('.content');
@@ -41,6 +44,32 @@ var app = app || {};
 			//save position
 			note.save({posx: offset.left, posy: offset.top});
 			return view;
+		},
+		dragStart: function (event) {
+			var left,
+			top;
+
+			if($(event.target).hasClass('sticky-note-container')){
+				this.dragItem = $(event.target);
+				left = this.dragItem.css('left');
+				top = this.dragItem.css('top');
+
+				event.originalEvent.dataTransfer.setData("text/plain", (left.replace(/px/,'') - event.clientX) + ',' + (top.replace(/px/,'') - event.clientY));
+			}
+		},
+		dragOver: function (event) {
+			event.preventDefault(); 
+			return false; 
+		},
+		dragDrop: function (event) {
+		if($(event.target)){
+			console.log('in2',event);
+			var offset = event.originalEvent.dataTransfer.getData("text/plain").split(',');
+			this.dragItem.css('left', (event.clientX + parseInt(offset[0],10)) + 'px');
+			this.dragItem.css('top' , (event.clientY + parseInt(offset[1],10)) + 'px');
+			event.preventDefault();
+			return false;
+		}
 		}
 	})	
 })();
